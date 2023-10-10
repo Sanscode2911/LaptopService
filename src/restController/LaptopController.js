617d7476acac322392da5d1a36a288c42595c12b
Associate <Associate@roifmrwindt>
2023-10-10 15:44:53 +0530

    final api

--- src/restController/LaptopController.js
+++ src/restController/LaptopController.js
@@ -13,48 +13,65 @@ const location_laptop_tax = [0.08, 0.23, 0.18];
 
 const laptop = JSON.parse(data);
 
-exports.getLaptopDetails = (req,res) => {
+exports.getLaptopDetails = (req, res) => {
     let loc = req.query.location;
-   let laptops;
+    let laptops;
 
-if (locations.includes(loc)) {
-    if (locations.indexOf(loc) == 0) {
-      laptops = JSON.parse(JSON.stringify(laptop));
-      laptops.forEach((e) => {
-        e.price = e.price + 0.08 * e.price;
-        e.price = e.price.toFixed(3);
-      });
-    } 
-
-      else if (locations.indexOf(loc) == 1) {
+    if (locations.includes(loc)) {
         laptops = JSON.parse(JSON.stringify(laptop));
-        laptops.forEach((e) => {
-          e.price = e.price + 0.23 * e.price;
-          e.price = e.price.toFixed(3);
-        });
-      } 
-      else if (locations.indexOf(loc) == 2) {
-        laptops = JSON.parse(JSON.stringify(laptop));
-        laptops.forEach((e) => {
-          e.price = e.price * 82.2; //Convert to INR
-          e.price = e.price + 0.18 * e.price;
-          e.price = e.price.toFixed(3);
-        });
-      }
-      res.status(200).json({
-        error: false,
-        message: "successful retrieval!",
-        data: laptops,
-      });
 
-}
+        // Apply location-specific logic
+        if (locations.indexOf(loc) === 0) {
+            laptops.forEach((e) => {
+                e.price = e.price + 0.08 * e.price;
+                e.price = e.price.toFixed(3);
+            });
+        } else if (locations.indexOf(loc) === 1) {
+            laptops.forEach((e) => {
+                e.price = e.price + 0.23 * e.price;
+                e.price = e.price.toFixed(3);
+            });
+        } else if (locations.indexOf(loc) === 2) {
+            laptops.forEach((e) => {
+                e.price = e.price * 82.2; // Convert to INR
+                e.price = e.price + 0.18 * e.price;
+                e.price = e.price.toFixed(3);
+            });
+        }
 
-else {
-    res.status(404).json({
-      error: true,
-      message: "unsuccessful get request!",
-      data: null,
-    });
-  }
+        // Optional parameters
+        const minPrice = parseFloat(req.query.minprice);
+        const maxPrice = parseFloat(req.query.maxprice);
+        const rating = parseFloat(req.query.rating);
+        const brand = req.query.brand;
+
+        // Filter laptops based on optional parameters
+        if (!isNaN(minPrice)) {
+            laptops = laptops.filter(e => e.price >= minPrice);
+        }
+
+        if (!isNaN(maxPrice)) {
+            laptops = laptops.filter(e => e.price <= maxPrice);
+        }
+
+        if (!isNaN(rating)) {
+            laptops = laptops.filter(e => e.rating >= rating);
+        }
+
+        if (brand) {
+            laptops = laptops.filter(e => e.brand.toLowerCase() === brand.toLowerCase());
+        }
+
+        res.status(200).json({
+            error: false,
+            message: "successful retrieval!",
+            data: laptops,
+        });
+    } else {
+        res.status(404).json({
+            error: true,
+            message: "unsuccessful get request!",
+            data: null,
+        });
+    }
 };
-
